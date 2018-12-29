a5f934e44d3c6767feed8d52205be0fed4f3dc48
kamiaki <247557768@qq.com>
2018-12-29 18:29:39 +0800

    Initial commit

---
+++ src/main/java/com/aki/websocket/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.aki.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by Roro on 2018/9/27.
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
